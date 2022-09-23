3270b83f0ffc51f056ab1d70abc5734ce4df889e
KeenRosal <keen@ethika.com>
2022-09-23 01:51:09 +0000

    added logic for sorting and paginations on static table. Currently is not synced to existing table

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="table table-bordered table-stripped mr-20">
+    <table id="listOfGames" class="table table-bordered table-stripped mr-20">
         <thead>
           <tr class="text-center bg-dark text-light">
             <th>Publisher</th>
@@ -26,17 +26,64 @@
 
 <script>
 export default {
-  name: 'GameList',
+  el: '#listOfGames',
+  //name: 'GameList',
+  //---------- Sorting and Pagination addition below -----------//
+  // CURRENTLY DOESN"T WORK.. 
   data () {
     return {
+      currentPage: 1,
+      elementsPerPage: 2,
+      ascending: false,
+      sortColumn: '',
     }
   },
+  methods: {
+    "sortTable": function sortTable(col) {
+      if (this.sortColumn === col) {
+        this.ascending = !this.ascending;
+      } else {
+        this.ascending = true;
+        this.sortColumn = col;
+      }
+
+      var ascending = this.ascending;
+
+      this.rows.sort(function(a, b) {
+        if (a[col] > b[col]) {
+          return ascending ? 1 : -1
+        } else if (a[col] < b[col]) {
+          return ascending ? -1 : 1
+        }
+        return 0;
+      })
+    },
+    "num_pages": function num_pages() {
+      return Math.ceil(this.rows.length / this.elementsPerPage);
+    },
+    "get_rows": function get_rows() {
+      var start = (this.currentPage-1) * this.elementsPerPage;
+      var end = start + this.elementsPerPage;
+      return this.rows.slice(start, end);
+    },
+    "change_page": function change_page(page) {
+      this.currentPage = page;
+    }
+  },
+  computed: {
+    "columns": function columns() {
+      if (this.rows.length == 0) {
+        return [];
+      }
+      return Object.keys(this.rows[0])
+    }
+  },
+  //---------- Sorting and Pagination addition above -----------//
   props: {
     newgames: {
       type: Array,
       required: true
     }
   },
-  
 }
 </script>
